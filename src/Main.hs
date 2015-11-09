1522c8f766c11fe593810749bd5dbea4bd56c738
Jim Burton <j.burton@brighton.ac.uk>
2015-11-09 21:43:09 +0000

    Tidyup implementing hlint suggestions.

--- src/Main.hs
+++ src/Main.hs
@@ -50,12 +50,12 @@ randInt n = getStdRandom (randomR (0, n))
 {-| Generate 3 distinct random ints between 0 and n. -} 
 threeRandInts :: Int -> IO (Int, Int, Int)
 threeRandInts n = do g <- newStdGen
-                     let [x, y, z] = take 3 . nub $ (randomRs (0, n) g)
+                     let [x, y, z] = take 3 . nub $ randomRs (0, n) g
                      return (x, y, z)
 
 {-| Lookup table for character substitutions. -}
-subst_table :: [(Char, Char)]
-subst_table = [ ('e', '3')
+substTable :: [(Char, Char)]
+substTable = [ ('e', '3')
               , ('a', '@')
               , ('l', '1')
               , ('s', '5')
@@ -64,7 +64,7 @@ subst_table = [ ('e', '3')
 {-| Substitute `c' by looking it up in the lookup table. If `c' is not
 present in the lookup table, return `c'. -} 
 subst :: Char -> Char
-subst c = case lookup (toLower c) subst_table of
+subst c = case lookup (toLower c) substTable of
             (Just c') -> c'
             Nothing   -> c
 
@@ -72,17 +72,17 @@ subst c = case lookup (toLower c) subst_table of
 maybeUpper :: Char -> IO Char
 maybeUpper c = do f <- randFloat
                   let i = round f
-                  if i == 0 then return $ toUpper c else return c
+                  return (if i == 0 then toUpper c else c)
 
 {-| Create a password according to the flags supplied. -}                  
 mkPasswd :: [Flag] -> IO String
 mkPasswd fs = 
-    do let h  = elem Help fs
-           w  = elem Wordy fs
-           v  = elem Version fs
-           s  = elem Strong fs
-           x  = elem VeryStrong fs
-           e  = elem Explain fs
+    do let h  = Help `elem` fs
+           w  = Wordy `elem` fs
+           v  = Version `elem` fs
+           s  = Strong `elem` fs
+           x  = VeryStrong `elem` fs
+           e  = Explain `elem` fs
            l  = getLen fs
            n  = min maxLength (max defaultLength l)
            fp = getFp fs 
@@ -109,27 +109,27 @@ mkPasswdFromDict n fp e =
     do str <- readFile fp
        let ls = filter ((==n) . length) (lines str)
        if null ls 
-        then error ("There are no words which are "++(show n)++" letters long in the file "++fp)
+        then error ("There are no words which are " ++ show n ++ " letters long in the file "++fp)
         else do i  <- randInt $ length ls
                 let w = ls!!i
-                w' <- sequence $ map (maybeUpper . subst) w
-                if e then return (w' ++ " [" ++ w ++ "]") else return w' 
+                w' <- mapM (maybeUpper . subst) w
+                return (if e then w' ++ " [" ++ w ++ "]" else w')
 
 {-| Create a password by concatenating three words from the dictionary file. -}
 mkPasswdW :: FilePath -> IO String
 mkPasswdW f = do str <- readFile f
                  let ls = lines str
-                     n  = (length ls) - 1
+                     n  = length ls - 1
                  (i1, i2, i3) <- threeRandInts n
                  return $ (ls !! i1) ++ (ls !! i2) ++ (ls !! i3)
                
 {-| Decide what to do based on the flags supplied. This will result in printing
 the help or version messages, or creating a password. -}
 processFlags :: [Flag] -> IO ()
-processFlags fs | elem Help fs    = putStrLn $ usageInfo header options
-                | elem Version fs = putStrLn version
-                | otherwise       = do pwd <- mkPasswd fs
-                                       putStrLn pwd
+processFlags fs | Help `elem` fs    = putStrLn $ usageInfo header options
+                | Version `elem` fs = putStrLn version
+                | otherwise         = do pwd <- mkPasswd fs
+                                         putStrLn pwd
 
 {-| Entry point. -}
 main :: IO ()
@@ -164,18 +164,18 @@ wordyLength   = 4
 version = "mkPasswd 0.1"
 
 options :: [OptDescr Flag] 
-options = [ Option ['l'] [] (ReqArg Length "6") "length of the password" 
-          , Option ['s'] [] (NoArg Strong) 
+options = [ Option "l" [] (ReqArg Length "6") "length of the password" 
+          , Option "s" [] (NoArg Strong) 
                                     "create a somewhat stronger password"
-          , Option ['w'] [] (NoArg Wordy) 
+          , Option "w" [] (NoArg Wordy) 
                                     "create a password from the concatenation \
                                     \of 3 words, e.g. correcthorsebattery"
-          , Option ['x'] [] (NoArg VeryStrong) "create a very strong password"
-          , Option ['h', '?'] [] (NoArg Help) "show this message"
-          , Option ['v'] [] (NoArg Version) "display the version number" 
-          , Option ['f'] [] (ReqArg WordsFile defaultWords) 
+          , Option "x" [] (NoArg VeryStrong) "create a very strong password"
+          , Option "h?" [] (NoArg Help) "show this message"
+          , Option "v" [] (NoArg Version) "display the version number" 
+          , Option "f" [] (ReqArg WordsFile defaultWords) 
                                     "location of the words file" 
-          , Option ['e'] [] (NoArg Explain) 
+          , Option "e" [] (NoArg Explain) 
                                     "if the password is based on a dictionary \
                                     \word, show the original word to make the \
                                     \password easier to remember" ]
